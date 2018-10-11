977b2371bf65f75a4e50b74085ff1f9bcf690111
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-10-11 13:38:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,5 +12,16 @@ class Student
     self
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
 
 end
